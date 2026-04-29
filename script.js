b71cb3efb9042e3a90c224e56c5c5f5adc9dccf1
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-29 11:30:31 -0300

    add - usuario

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("megaman").addEventListener("submit", function (e){
+e.preventDefault();
+
+let usuario = "man";
+let usuario = document.getElementById("usuario").value;
+
+})
+
